ca7d8d2a440a3946e39b9bb94900fc3a0e6acf36
leolana <leolucas.lana@hotmail.com>
2019-01-12 01:16:03 -0200

    Simplified movie controller

--- src/interfaces/rest/controllers/MovieController.ts
+++ src/interfaces/rest/controllers/MovieController.ts
@@ -1,44 +1,34 @@
 import * as express from 'express';
-import { inject } from 'inversify';
-import { controller, httpGet, httpPost, requestBody, requestParam, response } from 'inversify-express-utils';
+import { controller, httpGet, httpPost, requestBody, response } from 'inversify-express-utils';
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from 'http-status';
+
+import { Movie } from '../../../domain/entities/movie';
 
 @controller('/api/v1/movies')
 export class MovieController {
-  private readonly _movieRepository: Repository<Movie>;
-  public constructor(@inject(TYPE.MovieRepository) movieRepository: Repository<Movie>) {
-    this._movieRepository = movieRepository;
-  }
   @httpGet('/')
   public async get(@response() res: express.Response) {
     try {
-      return await this._movieRepository.find();
+      return [
+        { id: 1, title: 'Filme 1' },
+        { id: 2, title: 'Filme 2' },
+      ];
     } catch (e) {
-      res.status(500);
+      res.status(INTERNAL_SERVER_ERROR);
       res.send(e.message);
     }
   }
-  @httpGet('/:year')
-  public async getByYear(@response() res: express.Response, @requestParam('year') yearParam: string) {
-    try {
-      const year = parseInt(yearParam);
-      return await this._movieRepository.find({
-        year
-      });
-    } catch (e) {
-      res.status(500);
-      res.send(e.message);
-    }
-  }
+
   @httpPost('/')
   public async post(@response() res: express.Response, @requestBody() newMovie: Movie) {
     if (!(typeof newMovie.title === 'string') || isNaN(newMovie.year)) {
-      res.status(400);
+      res.status(BAD_REQUEST);
       res.send('Invalid Movie!');
     }
     try {
-      return await this._movieRepository.save(newMovie);
+      return { id: 2, title: 'Filme 2' };
     } catch (e) {
-      res.status(500);
+      res.status(INTERNAL_SERVER_ERROR);
       res.send(e.message);
     }
   }
